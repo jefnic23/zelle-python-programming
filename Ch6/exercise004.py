5a9553e877a1885215c9eda4d2be6bd286cc7069
Jeff Nicholas <jefnic23@gmail.com>
2020-03-22 20:16:18 -0400

    Update exercise004.py

--- Ch6/exercise004.py
+++ Ch6/exercise004.py
@@ -1,19 +1,21 @@
 nat_nums = eval(input('Enter a whole number: '))
 
-def sumN(nat_nums):
+
+def sum_n(nat_nums):
     n = 0
-    for i in range(nat_nums+1):
+    for i in range(nat_nums + 1):
         n += i
 
     return n
 
-def sumNCubes(nat_nums):
+
+def sum_n_cubes(nat_nums):
     n = 0
-    for i in range(nat_nums+1):
+    for i in range(nat_nums + 1):
         n += i ** 3
 
     return n
 
-print(sumN(nat_nums))
-print(sumNCubes(nat_nums))
 
+print(sum_n(nat_nums))
+print(sum_n_cubes(nat_nums))
