40ec62143596def3cb76c0727c1ced1e66c73f41
Jeff Nicholas <jefnic23@gmail.com>
2020-03-22 20:10:22 -0400

    Update exercise003.py

--- Ch6/exercise003.py
+++ Ch6/exercise003.py
@@ -2,11 +2,14 @@ import math
 
 radius = eval(input('Enter sphere radius: '))
 
-def sphereArea(radius):
+
+def sphere_area(radius):
     return 4 * math.pi * radius ** 2
 
-def sphereVolume(radius):
+
+def sphere_volume(radius):
     return 4 / 3 * math.pi * radius ** 3
 
-print(sphereArea(radius))
-print(sphereVolume(radius))
+
+print(sphere_area(radius))
+print(sphere_volume(radius))
