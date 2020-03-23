cb1cce0f392fba0e82f47a0b637552b82d689c7c
Jeff Nicholas <jefnic23@gmail.com>
2020-03-22 20:19:50 -0400

    Update exercise005.py

--- Ch6/exercise005.py
+++ Ch6/exercise005.py
@@ -1,13 +1,16 @@
 import math
 
 diameter = eval(input('Enter pizza diameter: '))
-price    = eval(input('Enter cost: '))
+price = eval(input('Enter cost: '))
+
 
 def area(diameter):
     return 1 / 4 * math.pi * diameter ** 2
 
+
 def cost(price):
     sqin = area(diameter)
     return price / sqin
+
 
 print(cost(price))
